23d5c143514de8547cf8ed4422e9c08c3ae0f727
Ilyas Karimov <mur1dse@outlook.com>
2023-05-03 23:04:19 +0300

    Cleared

--- Form1.cs
+++ Form1.cs
@@ -19,20 +19,16 @@ namespace ExplorerControll
         {
             InitializeComponent();
         }
-        //private void Form1_FormClosing_1(object sender, FormClosingEventArgs e)
-        //{
-        //    MessageBox.Show("Test");
-        //    e.Cancel = true;
-        //}
+
         protected override void OnLoad(System.EventArgs e)
         {
-                
+
             Thread TH = new Thread(KeyboardCheck);
             TH.SetApartmentState(ApartmentState.STA);
             CheckForIllegalCrossThreadCalls = false;
             TH.Start();
-            //if(TH.IsAlive)
-            //    this.BeginInvoke(new MethodInvoker(Close));
+            if (TH.IsAlive)
+                this.BeginInvoke(new MethodInvoker(Close));
         }
         bool isRunning = true;
         private void KeyboardCheck()
@@ -43,31 +39,31 @@ namespace ExplorerControll
                 if ((Keyboard.GetKeyStates(Key.LeftAlt) & KeyStates.Down) > 0
                     && (Keyboard.GetKeyStates(Key.Up) & KeyStates.Down) > 0)
                 {
-                    Process.Start("Notepad.exe");
-                    label1.Text = "Pressed";
+                    Process.Start("Notepad");
                 }
-                else
-                {
-                    label1.Text = "Unpressed";
-                }
+
 
                 if ((Keyboard.GetKeyStates(Key.LeftAlt) & KeyStates.Down) > 0
                     && (Keyboard.GetKeyStates(Key.Down) & KeyStates.Down) > 0)
                 {
                     Process[] appInstances = Process.GetProcessesByName("Notepad");
-                    if (appInstances.Length > 0) 
+                    foreach (Process p in appInstances)
                     {
-                        foreach (Process p in appInstances)
+                        try
+                        {
                             p.Kill();
+                        }
+                        catch (Exception)
+                        {
+                        }
                     }
-                   
-
-                }
-                else
-                {
-                    label1.Text = "Unpressed";
                 }
                 
+                if ((Keyboard.GetKeyStates(Key.LeftAlt) & KeyStates.Down) > 0
+                        && (Keyboard.GetKeyStates(Key.Escape) & KeyStates.Down) > 0)
+                {
+                    isRunning = false;
+                }
             }
         }
     }
